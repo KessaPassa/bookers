760cfe95c3190f45e04bb4efc53ae7fe7c6985a7
KessaPassa <k.someya.gyao@gmail.com>
2023-05-25 02:27:38 +0900

    run: rubocop -A --disable-uncorrectable

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = 'Book was successfully created.'
+      flash[:notice] = 'Book was successfully created.' # rubocop:todo Rails/I18nLocaleTexts
       redirect_to book_path(@book.id)
     else
       render :index
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update
-      flash[:notice] = 'Book was successfully updated.'
+      flash[:notice] = 'Book was successfully updated.' # rubocop:todo Rails/I18nLocaleTexts
       redirect_to book_path(@book.id)
     else
       render :edit
